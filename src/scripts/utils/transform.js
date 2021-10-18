c68dfd34910f59a4d5dee787039a6944527f3842
Jacob Durham <jfdurham88@gmail.com>
2021-10-18 00:31:31 -0600

    comments in transform - seems like it can go

--- src/scripts/utils/transform.js
+++ src/scripts/utils/transform.js
@@ -1,16 +1,18 @@
+// should be able to handle this with compile time tools
 export function transform(el, transformValue) {
     el.style.webkitTransform = transformValue;
     el.style.msTransform = transformValue;
     el.style.transform = transformValue;
 }
 
+// and this?
 export function getTranslate(el) {
     const translate = {};
-    if (!window.getComputedStyle) return;
 
     const style = getComputedStyle(el);
     const transform = style.transform || style.webkitTransform || style.mozTransform;
 
+    //HMMMM
     let mat = transform.match(/^matrix3d\((.+)\)$/);
     if (mat) {
         translate.x = mat ? parseFloat(mat[1].split(', ')[12]) : 0;
