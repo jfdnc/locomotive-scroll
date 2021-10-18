911bfb8746d386d5eaf17082f1e620d69450501c
Jacob Durham <jfdurham88@gmail.com>
2021-10-18 00:31:11 -0600

    beef up lerp fn

--- src/scripts/utils/maths.js
+++ src/scripts/utils/maths.js
@@ -1,3 +1,13 @@
-export function lerp(start, end, amt) {
-    return (1 - amt) * start + amt * end;
+/**
+ * linear interpolation between `start` and `end` values
+ * 
+ * @param {number} start
+ * @param {number} end 
+ * @param {number} pct in range 0..1
+ * @returns {number} interpolated value
+ */
+export function lerp(start = 0, end = 0, pct = 0.0) {
+    if(pct < 0 || pct > 1) return 0; // handle outside points
+    // clamp return value
+    return (1 - pct) * start + pct * end;
 }
